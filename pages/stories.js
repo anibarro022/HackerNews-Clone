fbf013db5aebdf4d43fdabde62b5de0ae35e4a40
Nico <126928196+anibarro022@users.noreply.github.com>
2023-11-17 00:38:41 -0800

    Add story formatting

--- pages/stories.js
+++ pages/stories.js
@@ -1,3 +1,4 @@
+import Story from '../components/Story.js';
 import view from '../utils/view.js';
 
 export default async function Stories(path) {
@@ -5,7 +6,7 @@ export default async function Stories(path) {
     const hasStories = stories.length > 0;
 
     view.innerHTML = `<div>
-    ${hasStories ? stories.map(story => JSON.stringify(story)) : 'No Stories'}
+    ${hasStories ? stories.map((story, i) => Story({ ...story, index: i + 1 })).join('') : 'No Stories'}
     </div>`;  
 }
 
